fca730739fe15bce118a7108863ab13065ca747b
sebastian alarcon ochoa <sebastian.alarcon.ext@kavak.com>
2023-11-23 21:06:01 -0500

    feat: add invoice erquest

--- src/medical-appointment/medical-appointment.controller.ts
+++ src/medical-appointment/medical-appointment.controller.ts
@@ -154,6 +154,8 @@ export class MedicalAppointmentController {
   async delete(
     @Body() medicalAppointmentParams: DeleteQueryParams,
   ): Promise<IDeleteReturn> {
+    console.log('medicalAppointmentParams: ', medicalAppointmentParams)
+
     try {
       const { idAffiliate, idAppointment } = await deleteSchema.validateAsync(
         medicalAppointmentParams,
